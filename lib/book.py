35d99b8b71449dfd5b9c5c69163cf600701b3c54
O-liverr <oliekoech@gmail.com>
2025-05-19 15:26:27 +0300

    p3-oop-putting-it-all-together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,49 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self._title = title
+        self._page_count = page_count
+        self._is_checked_out = False
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            print("Title must be a string")
+            return
+        self._title = value
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            return
+        if value <= 0:
+            print("Page count must be a positive integer")
+            return
+        self._page_count = value
+
+    @property
+    def is_checked_out(self):
+        return self._is_checked_out
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return "Book checked out"
+        return "Book already checked out"
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return "Book returned"
+        return "Book is not checked out"
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,38 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if not isinstance(value, str):
+            print("Brand must be a string")
+            return
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            return
+        if value <= 0:
+            print("Size must be a positive number")
+            return
+        self._size = value
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def cobble(self):
+        self._condition = "New"
+        print("Your shoe is as good as new!")
